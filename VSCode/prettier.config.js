def28838bdee430a915c7f8e2c44396cc882018a
SimmeNilsson <30928046+SimmeNilsson@users.noreply.github.com>
2024-11-11 14:16:13 +0100

    Create prettier.config.js

---
+++ VSCode/prettier.config.js
@@ -0,0 +1,9 @@
+export default {
+    arrowParens: "avoid",
+    endOfLine: "auto",
+    htmlWhitespaceSensitivity: "css",
+    printWidth: 120,
+    quoteProps: "consistent",
+    tabWidth: 4,
+    trailingComma: "none"
+}
